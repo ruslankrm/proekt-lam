0cd89330f86919b84614e64bd82a051371c2f85e
ruslan <ruslankrm@bigmir.net>
2018-10-09 12:46:12 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const previous = document.getElementsByClassName('previous');
+const next = document.getElementsByClassName('next');
+
+
+
+right.onclick = function(){
+    
+}
+
+
+next.onclick = function(){
+    
+}
+
